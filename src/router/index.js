ccb953cbce6dd6811f750680fe831a45be9690c4
Reyes-Tec <r.c.c.t0107@gmail.com>
2024-11-12 19:32:51 -0600

    modificacion de ruta2

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HistoriaPage from '../views/HistoriaPage.vue'
-import GaleriaPage from '../views/GaleriaPage.vue'
+
 import SoportePage from '../views/SoportePage.vue'
 import HomeView from '@/views/HomeView.vue'
 import GaleryPage from '@/views/GaleryPage.vue'
